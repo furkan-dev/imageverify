1869941aaeda96542cb70edf2630e600e05df60d
Furkan Akal <30433306+furkan-dev@users.noreply.github.com>
2017-08-01 15:45:50 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+//
+//
+// 29.08.17 saturday
+// coded by 'furkan-dev'
+//
+//
+
 <!-- Tarayıcı ön belleğinde tutulmaması için no-cache -->
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//TR" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="TR" lang="tr">
